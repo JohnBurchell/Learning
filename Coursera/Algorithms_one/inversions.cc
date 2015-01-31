6c98aedb6a7c67bee0df665b85abf5ac41b8c9fb
John Burchell <john.a.burchell@gmail.com>
2015-01-31 19:13:25 +0100

    Added Algorithms work for coursera course

---
+++ Coursera/Algorithms_one/inversions.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int count(int& a, int len)
+{
+	if(len == 1)
+	{
+		return 0;
+	}
+
+	auto x = count(a, len / 2);
+	auto y = count(a, len / 2);
+	//
+}
+
+std::vector<int> read_file()
+{
+	fstream	input;
+	input.open("test.txt");
+
+	string input_string;
+	while(getline(input, input_string))
+	{
+
+	}
+}
+
+int inversions(int& one, int& two)
+{
+	return 0;
+}
+
+int main()
+{
+
+}
